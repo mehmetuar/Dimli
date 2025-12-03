2085ca3945ffde79fb5bcfb6d303e3798a34c5d5
mehmetuar <mehmetuar@hotmail.com>
2025-12-03 10:08:55 +0300

    fix: Re-add losses column to Team entity

--- server/src/teams/team.entity.ts
+++ server/src/teams/team.entity.ts
@@ -29,6 +29,10 @@ export class Team {
 
     @Column('int', { default: 0 })
     wins: number;
+
+    @Column('int', { default: 0 })
+    losses: number;
+
     // Captain ID for easy access
     @Column({ nullable: true })
     captainId: string;
