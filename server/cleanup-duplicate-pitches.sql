36076e3852bc52abc93a71e2b78cfb248fa23ddc
mehmetuar <mehmetuar@hotmail.com>
2026-04-30 19:25:12 +0300

    refactor: server tarafında kullanılmayan scriptler, dummy veriler ve boş klasörler temizlendi

--- server/cleanup-duplicate-pitches.sql
+++
@@ -1,8 +0,0 @@
--- Kadıköy Arena için eski "Merkez Saha" pitch'ini silme
--- Bu pitch eski seed'den kalmış ve tutarsızlık yaratıyor
-
--- İlk olarak bu pitch ile ilişkili reservations ve match announcements'ları kontrol et
-SELECT id, name, business_id FROM pitch WHERE name = 'Merkez Saha';
-
--- Eğer ilişkili kayıtlar yoksa sil
--- DELETE FROM pitch WHERE name = 'Merkez Saha' AND business_id IN (SELECT id FROM business WHERE name = 'Kadıköy Arena');
